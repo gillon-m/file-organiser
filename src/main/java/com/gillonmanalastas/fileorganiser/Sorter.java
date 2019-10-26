00c2c457ebdcbcf17504269dbdc1207d8a79be65
Gillon Manalastas <gman485@aucklanduni.ac.nz>
2019-10-26 21:31:25 +1100

    duplicate files fix

--- src/main/java/com/gillonmanalastas/fileorganiser/Sorter.java
+++ src/main/java/com/gillonmanalastas/fileorganiser/Sorter.java
@@ -31,7 +31,7 @@ public class Sorter {
         ProgressBar progressBar = new ProgressBar(files.size(), LOAD_LENGTH);
         for (File file : files) {
             System.out.print(progressBar.increment(file.getName()) + "\r");
-            if (!(new File(outputDirectory+File.separator+file.getName()).exists())) {
+            if (!doesFileExistOutputDirectory(file)) {
                 FileUtils.copyFileToDirectory(file, new File(outputDirectory));
             } else {
                 saveDuplicate(file);
@@ -44,10 +44,18 @@ public class Sorter {
         Integer count = duplicatesCount.get(filename);
         if (count == null) {
             duplicatesCount.put(filename, 1);
-        } else {
-            duplicatesCount.put(filename, count + 1);
         }
         count = duplicatesCount.get(filename);
-        FileUtils.copyFile(file, new File(outputDirectory + File.separator + "copy" + count + "-"+file.getName()));
+        File copyFile = new File(outputDirectory + File.separator + "copy" + count + "-" + file.getName());
+        while (doesFileExistOutputDirectory(copyFile)) {
+            count++;
+            copyFile = new File(outputDirectory + File.separator + "copy" + count + "-" + file.getName());
+        }
+        duplicatesCount.put(filename, count);
+        FileUtils.copyFile(file, copyFile);
+    }
+
+    private boolean doesFileExistOutputDirectory(File file) {
+        return new File(outputDirectory + File.separator + file.getName()).exists();
     }
 }
