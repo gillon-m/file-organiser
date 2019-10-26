dc2c473012f5daffebb8b7661efc36a865dd061f
Gillon Manalastas <gman485@aucklanduni.ac.nz>
2019-10-26 21:09:27 +1100

    added functionality for saving duplicate files

--- src/main/java/com/gillonmanalastas/fileorganiser/Sorter.java
+++ src/main/java/com/gillonmanalastas/fileorganiser/Sorter.java
@@ -35,9 +35,9 @@ public class Sorter {
         ProgressBar progressBar = new ProgressBar(files.size(), LOAD_LENGTH);
         for (File file : files) {
             System.out.print(progressBar.increment(file.getName()) + "\r");
-            if (!file.exists()) {
+            if (!(new File(outputDirectory+File.separator+file.getName()).exists())) {
                 FileUtils.copyFileToDirectory(file, new File(outputDirectory));
-            }else{
+            } else {
                 saveDuplicate(file);
             }
         }
@@ -51,6 +51,7 @@ public class Sorter {
         } else {
             duplicatesCount.put(filename, count + 1);
         }
-        FileUtils.copyFileToDirectory(file, new File(outputDirectory +"-copy-"+count));
+        count = duplicatesCount.get(filename);
+        FileUtils.copyFile(file, new File(outputDirectory + File.separator + "copy" + count + "-"+file.getName()));
     }
 }
