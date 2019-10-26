03adf07701d4e73ab2a31417a064d3b22c88f7ea
Gillon Manalastas <gman485@aucklanduni.ac.nz>
2019-10-26 20:18:35 +1100

    added exception handle for no input

--- src/main/java/com/gillonmanalastas/fileorganiser/FileOrganiser.java
+++ src/main/java/com/gillonmanalastas/fileorganiser/FileOrganiser.java
@@ -9,10 +9,8 @@ import java.util.Collection;
 public class FileOrganiser{
     public static void main(String[] args) throws IOException {
 //        new Importer().importFiles(System.getProperty("user.dir"));
-//        String directoryToSort = System.getProperty("user.dir")+ File.separator+"to_sort";
-//        String sortedDirectory = System.getProperty("user.dir")+ File.separator+"sorted";
-        String directoryToSort = "C:\\Users\\Gillon\\IdeaProjects\\to_sort";
-        String sortedDirectory = "C:\\Users\\Gillon\\IdeaProjects\\sorted";
+        String directoryToSort = args[0];
+        String sortedDirectory = args[1];
         Collection<File> filesToSort = FileUtils.listFiles(new File(directoryToSort), null, true);
         ProgressBar progressBar = new ProgressBar(filesToSort.size(), 20);
         for(File file : filesToSort) {
